e1943c4b6729594b3e019ad044202e2a8df5d279
Kaiyuan Hou <khou2020@outlook.com>
2019-11-03 08:30:23 -0600

    H5Vprint2

--- src/H5V.c
+++ src/H5V.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include "mpi.h"
 #include "H5V.h"
+#include "H5public.h"
 
 static double eval_tlocal[EVAL_NTIMER];
 static double eval_clocal[EVAL_NTIMER];
@@ -10,93 +11,93 @@ static double eval_minlocal[EVAL_NMPI];
 static double eval_sumlocal[EVAL_NMPI];
 
 const char * const eval_tname[] = { 
-                                    "hdf5_eval_MPI_Allgather",
-                                    "hdf5_eval_MPI_Allgatherv",
-                                    "hdf5_eval_MPI_Allreduce",
-                                    "hdf5_eval_MPI_Bcast",
-                                    "hdf5_eval_MPI_Gather",
-                                    "hdf5_eval_MPI_Gatherv",
-                                    "hdf5_eval_MPI_Send",
-                                    "hdf5_eval_MPI_Isend",
-                                    "hdf5_eval_MPI_Recv",
-                                    "hdf5_eval_MPI_Imrecv",
-                                    "hdf5_eval_MPI_Mprobe",
-                                    "hdf5_eval_MPI_File_read_at",
-                                    "hdf5_eval_MPI_File_read_at_all",
-                                    "hdf5_eval_MPI_File_write_at",
-                                    "hdf5_eval_MPI_File_write_at_all",
-                                    "hdf5_eval_MPI_File_set_view",
-                                    "hdf5_eval_H5Fcreate",
-                                    "hdf5_eval_H5Fopen",
-                                    "hdf5_eval_H5Fclose",
-                                    "hdf5_eval_H5Gcreate",
-                                    "hdf5_eval_H5Gopen",
-                                    "hdf5_eval_H5Gclose",
-                                    "hdf5_eval_H5Dcreate",
-                                    "    hdf5_eval_H5D__create_named",
-                                    "        hdf5_eval_H5L_link_object_dataset",
-                                    "            hdf5_eval_H5L__create_real_dataset",
-                                    "                hdf5_eval_H5L__link_cb_dataset",
-                                    "                    hdf5_eval_H5O_obj_create_dataset",
-                                    "                        hdf5_eval_H5O__dset_create",
-                                    "                            hdf5_eval_H5D__create",
-                                    "                                hdf5_eval_H5D__create.metadata",
-                                    "                                hdf5_eval_H5D__create.property",
-                                    "                                hdf5_eval_H5D__chunk_construct",
-                                    "                                hdf5_eval_H5D__update_oh_info",
-                                    "                    hdf5_eval_H5G_obj_insert_dataset",
-                                    "hdf5_eval_H5Dopen",
-                                    "hdf5_eval_H5Dclose",
-                                    "hdf5_eval_H5Acreate",
-                                    "hdf5_eval_H5Aopen",
-                                    "hdf5_eval_H5Aclose",
-                                    "hdf5_eval_H5Dwrite", 
-                                    "    hdf5_eval_H5D__write", 
-                                    "        hdf5_eval_H5D__chunk_io_init_w", 
-                                    "        hdf5_eval_H5D__ioinfo_adjust_w",
-                                    "        hdf5_eval_H5D__chunk_collective_write", 
-                                    "             hdf5_eval_H5D__chunk_collective_io_w", 
-                                    "                 hdf5_eval_H5D__link_chunk_filtered_collective_io_w", 
-                                    "                     hdf5_eval_H5D__construct_filtered_io_info_list_w",
-                                    "                         hdf5_eval_H5D__chunk_redistribute_shared_chunks", 
-                                    "                             hdf5_eval_H5D__chunk_redistribute_shared_chunks.Chunk_assignmentw", 
-                                    "                             hdf5_eval_H5D__chunk_redistribute_shared_chunks:Data_exchange", 
-                                    "                     hdf5_eval_H5D__filtered_collective_chunk_entry_io_w",
-                                    "                         hdf5_eval_H5F_block_read@H5D__filtered_collective_chunk_entry_io_w(READ_BACK_PARTIAL_CHUNK)",
-                                    "                         hdf5_eval_H5D__filtered_collective_chunk_entry_io.Unfilter_w(DECOMPRESS_PARTIAL_CHUNK)",
-                                    "                         hdf5_eval_H5D__filtered_collective_chunk_entry_io.Self_w",
-                                    "                         hdf5_eval_H5D__filtered_collective_chunk_entry_io.Unpack_w",
-                                    "                         hdf5_eval_H5D__filtered_collective_chunk_entry_io.Filter_w(COMRPESSION)",
-                                    "                     hdf5_eval_H5D__link_chunk_filtered_collective_io.Chunk_Alloc_w",
-                                    "                     hdf5_eval_H5D__link_chunk_filtered_collective_io.Type_Create_w",
-                                    "                     hdf5_eval_H5D__final_collective_io_w(WRITE_TO_FILE)",
-                                    "                     hdf5_eval_H5D__link_chunk_filtered_collective_io.Update_Index_w",
-                                    "                 hdf5_eval_H5D__multi_chunk_filtered_collective_io_w",
-                                    "hdf5_eval_H5Dread",
-                                    "    hdf5_eval_H5D__read",
-                                    "        hdf5_eval_H5D__read.check_arg",
-                                    "        hdf5_eval_H5D__chunk_io_init_r", 
-                                    "        hdf5_eval_H5D__ioinfo_adjust_r",
-                                    "        hdf5_eval_H5D__chunk_collective_read(COLLECTIVE_READ)", 
-                                    "             hdf5_eval_H5D__chunk_collective_io_r", 
-                                    "                 hdf5_eval_H5D__multi_chunk_filtered_collective_io_r",
-                                    "                     hdf5_eval_H5D__construct_filtered_io_info_list_r",      
-                                    "                     hdf5_eval_H5D__filtered_collective_chunk_entry_io_r",  
-                                    "                         hdf5_eval_H5F_block_read@H5D__filtered_collective_chunk_entry_io_r(READ_RAW_DATA)",
-                                    "                         hdf5_eval_H5D__filtered_collective_chunk_entry_io.Unfilter_r(DECOMPRESSION)",
-                                    "                         hdf5_eval_H5D__filtered_collective_chunk_entry_io.Self_r",
-                                    "                 hdf5_eval_H5D__link_chunk_filtered_collective_io_r", 
-                                    "        hdf5_eval_H5D__chunk_read(INDEPENDENT_READ)",
-                                    "            hdf5_eval_H5D__chunk_lookup_r",
-                                    "            hdf5_eval_H5D__chunk_lock_r",
-                                    "                hdf5_eval_H5F_block_read@H5D__chunk_lock_r(READ_RAW_DATA)",
-                                    "                hdf5_eval_H5D__chunk_lock.Filter_r(DECOMPRESSION)",
-                                    "            hdf5_eval_H5D__select_read",
-                                    "            hdf5_eval_H5D__chunk_unlock_r",
-                                    "hdf5_eval_H5Ovisit",
-                                    "hdf5_eval_H5Ovisit2",
-                                    "hdf5_eval_H5Z_filter_deflate_comp",
-                                    "hdf5_eval_H5Z_filter_deflate_decomp",
+                                    "MPI_Allgather",
+                                    "MPI_Allgatherv",
+                                    "MPI_Allreduce",
+                                    "MPI_Bcast",
+                                    "MPI_Gather",
+                                    "MPI_Gatherv",
+                                    "MPI_Send",
+                                    "MPI_Isend",
+                                    "MPI_Recv",
+                                    "MPI_Imrecv",
+                                    "MPI_Mprobe",
+                                    "MPI_File_read_at",
+                                    "MPI_File_read_at_all",
+                                    "MPI_File_write_at",
+                                    "MPI_File_write_at_all",
+                                    "MPI_File_set_view",
+                                    "H5Fcreate",
+                                    "H5Fopen",
+                                    "H5Fclose",
+                                    "H5Gcreate",
+                                    "H5Gopen",
+                                    "H5Gclose",
+                                    "H5Dcreate",
+                                    "    H5D__create_named",
+                                    "        H5L_link_object_dataset",
+                                    "            H5L__create_real_dataset",
+                                    "                H5L__link_cb_dataset",
+                                    "                    H5O_obj_create_dataset",
+                                    "                        H5O__dset_create",
+                                    "                            H5D__create",
+                                    "                                H5D__create.metadata",
+                                    "                                H5D__create.property",
+                                    "                                H5D__chunk_construct",
+                                    "                                H5D__update_oh_info",
+                                    "                    H5G_obj_insert_dataset",
+                                    "H5Dopen",
+                                    "H5Dclose",
+                                    "H5Acreate",
+                                    "H5Aopen",
+                                    "H5Aclose",
+                                    "H5Dwrite", 
+                                    "    H5D__write", 
+                                    "        H5D__chunk_io_init_w", 
+                                    "        H5D__ioinfo_adjust_w",
+                                    "        H5D__chunk_collective_write", 
+                                    "             H5D__chunk_collective_io_w", 
+                                    "                 H5D__link_chunk_filtered_collective_io_w", 
+                                    "                     H5D__construct_filtered_io_info_list_w",
+                                    "                         H5D__chunk_redistribute_shared_chunks", 
+                                    "                             H5D__chunk_redistribute_shared_chunks.Chunk_assignmentw", 
+                                    "                             H5D__chunk_redistribute_shared_chunks:Data_exchange", 
+                                    "                     H5D__filtered_collective_chunk_entry_io_w",
+                                    "                         H5F_block_read@H5D__filtered_collective_chunk_entry_io_w(READ_BACK_PARTIAL_CHUNK)",
+                                    "                         H5D__filtered_collective_chunk_entry_io.Unfilter_w(DECOMPRESS_PARTIAL_CHUNK)",
+                                    "                         H5D__filtered_collective_chunk_entry_io.Self_w",
+                                    "                         H5D__filtered_collective_chunk_entry_io.Unpack_w",
+                                    "                         H5D__filtered_collective_chunk_entry_io.Filter_w(COMRPESSION)",
+                                    "                     H5D__link_chunk_filtered_collective_io.Chunk_Alloc_w",
+                                    "                     H5D__link_chunk_filtered_collective_io.Type_Create_w",
+                                    "                     H5D__final_collective_io_w(WRITE_TO_FILE)",
+                                    "                     H5D__link_chunk_filtered_collective_io.Update_Index_w",
+                                    "                 H5D__multi_chunk_filtered_collective_io_w",
+                                    "H5Dread",
+                                    "    H5D__read",
+                                    "        H5D__read.check_arg",
+                                    "        H5D__chunk_io_init_r", 
+                                    "        H5D__ioinfo_adjust_r",
+                                    "        H5D__chunk_collective_read(COLLECTIVE_READ)", 
+                                    "             H5D__chunk_collective_io_r", 
+                                    "                 H5D__multi_chunk_filtered_collective_io_r",
+                                    "                     H5D__construct_filtered_io_info_list_r",      
+                                    "                     H5D__filtered_collective_chunk_entry_io_r",  
+                                    "                         H5F_block_read@H5D__filtered_collective_chunk_entry_io_r(READ_RAW_DATA)",
+                                    "                         H5D__filtered_collective_chunk_entry_io.Unfilter_r(DECOMPRESSION)",
+                                    "                         H5D__filtered_collective_chunk_entry_io.Self_r",
+                                    "                 H5D__link_chunk_filtered_collective_io_r", 
+                                    "        H5D__chunk_read(INDEPENDENT_READ)",
+                                    "            H5D__chunk_lookup_r",
+                                    "            H5D__chunk_lock_r",
+                                    "                H5F_block_read@H5D__chunk_lock_r(READ_RAW_DATA)",
+                                    "                H5D__chunk_lock.Filter_r(DECOMPRESSION)",
+                                    "            H5D__select_read",
+                                    "            H5D__chunk_unlock_r",
+                                    "H5Ovisit",
+                                    "H5Ovisit2",
+                                    "H5Z_filter_deflate_comp",
+                                    "H5Z_filter_deflate_decomp",
                                     };
 
 
@@ -137,13 +138,17 @@ herr_t H5Venable(){
     eval_enable = 1;
     return 0;
 }
+
 herr_t H5Vdisable(){
     eval_enable = 0;
     return 0;
 }
 
-// Note: This only work if everyone calls H5Fclose
 herr_t H5Vprint(){
+    return H5Vprint2("hdf5_eval_");
+}
+
+herr_t H5Vprint2(char *prefix){
     int i;
     int np, rank, flag;
     double tmax[EVAL_NTIMER], tmin[EVAL_NTIMER], tmean[EVAL_NTIMER], tvar[EVAL_NTIMER], tvar_local[EVAL_NTIMER];
@@ -167,10 +172,10 @@ herr_t H5Vprint(){
 
     if (rank == 0){
         for(i = 0; i < EVAL_NTIMER; i++){
-            printf("#+$: %s_time_mean: %lf\n", eval_tname[i], tmean[i]);
-            printf("#+$: %s_time_max: %lf\n", eval_tname[i], tmax[i]);
-            printf("#+$: %s_time_min: %lf\n", eval_tname[i], tmin[i]);
-            printf("#+$: %s_time_var: %lf\n\n", eval_tname[i], tvar[i]);
+            printf("#+$: %s%s_time_mean: %lf\n", prefix, eval_tname[i], tmean[i]);
+            printf("#+$: %s%s_time_max: %lf\n", prefix, eval_tname[i], tmax[i]);
+            printf("#+$: %s%s_time_min: %lf\n", prefix, eval_tname[i], tmin[i]);
+            printf("#+$: %s%s_time_var: %lf\n\n", prefix, eval_tname[i], tvar[i]);
         }
     }
 
@@ -185,10 +190,10 @@ herr_t H5Vprint(){
 
     if (rank == 0){
         for(i = 0; i < EVAL_NTIMER; i++){
-            printf("#+$: %s_count_mean: %lf\n", eval_tname[i], tmean[i]);
-            printf("#+$: %s_count_max: %lf\n", eval_tname[i], tmax[i]);
-            printf("#+$: %s_count_min: %lf\n", eval_tname[i], tmin[i]);
-            printf("#+$: %s_count_var: %lf\n\n", eval_tname[i], tvar[i]);
+            printf("#+$: %s%s_count_mean: %lf\n", prefix, eval_tname[i], tmean[i]);
+            printf("#+$: %s%s_count_max: %lf\n", prefix, eval_tname[i], tmax[i]);
+            printf("#+$: %s%s_count_min: %lf\n", prefix, eval_tname[i], tmin[i]);
+            printf("#+$: %s%s_count_var: %lf\n\n", prefix, eval_tname[i], tvar[i]);
         }
     }
 
@@ -203,10 +208,10 @@ herr_t H5Vprint(){
 
     if (rank == 0){
         for(i = 0; i < EVAL_NMPI; i++){
-            printf("#+$: %s_size_sum_mean: %lf\n", eval_tname[i], tmean[i]);
-            printf("#+$: %s_size_sum_max: %lf\n", eval_tname[i], tmax[i]);
-            printf("#+$: %s_size_sum_min: %lf\n", eval_tname[i], tmin[i]);
-            printf("#+$: %s_size_sum_var: %lf\n\n", eval_tname[i], tvar[i]);
+            printf("#+$: %s%s_size_sum_mean: %lf\n", prefix, eval_tname[i], tmean[i]);
+            printf("#+$: %s%s_size_sum_max: %lf\n", prefix, eval_tname[i], tmax[i]);
+            printf("#+$: %s%s_size_sum_min: %lf\n", prefix, eval_tname[i], tmin[i]);
+            printf("#+$: %s%s_size_sum_var: %lf\n\n", prefix, eval_tname[i], tvar[i]);
         }
     }
 
@@ -221,10 +226,10 @@ herr_t H5Vprint(){
 
     if (rank == 0){
         for(i = 0; i < EVAL_NMPI; i++){
-            printf("#+$: %s_size_max_mean: %lf\n", eval_tname[i], tmean[i]);
-            printf("#+$: %s_size_max_max: %lf\n", eval_tname[i], tmax[i]);
-            printf("#+$: %s_size_max_min: %lf\n", eval_tname[i], tmin[i]);
-            printf("#+$: %s_size_max_var: %lf\n\n", eval_tname[i], tvar[i]);
+            printf("#+$: %s%s_size_max_mean: %lf\n", prefix, eval_tname[i], tmean[i]);
+            printf("#+$: %s%s_size_max_max: %lf\n", prefix, eval_tname[i], tmax[i]);
+            printf("#+$: %s%s_size_max_min: %lf\n", prefix, eval_tname[i], tmin[i]);
+            printf("#+$: %s%s_size_max_var: %lf\n\n", prefix, eval_tname[i], tvar[i]);
         }
     }
 
@@ -239,10 +244,10 @@ herr_t H5Vprint(){
 
     if (rank == 0){
         for(i = 0; i < EVAL_NMPI; i++){
-            printf("#+$: %s_size_min_mean: %lf\n", eval_tname[i], tmean[i]);
-            printf("#+$: %s_size_min_max: %lf\n", eval_tname[i], tmax[i]);
-            printf("#+$: %s_size_min_min: %lf\n", eval_tname[i], tmin[i]);
-            printf("#+$: %s_size_min_var: %lf\n\n", eval_tname[i], tvar[i]);
+            printf("#+$: %s%s_size_min_mean: %lf\n", prefix, eval_tname[i], tmean[i]);
+            printf("#+$: %s%s_size_min_max: %lf\n", prefix, eval_tname[i], tmax[i]);
+            printf("#+$: %s%s_size_min_min: %lf\n", prefix, eval_tname[i], tmin[i]);
+            printf("#+$: %s%s_size_min_var: %lf\n\n", prefix, eval_tname[i], tvar[i]);
         }
     }
 
--- src/H5Vpublic.h
+++ src/H5Vpublic.h
@@ -9,6 +9,7 @@ extern "C" {
 H5_DLL herr_t H5Venable(void);
 H5_DLL herr_t H5Vdisable(void);
 H5_DLL herr_t H5Vprint(void);
+H5_DLL herr_t H5Vprint2(char*)
 H5_DLL herr_t H5Vreset(void);
 
 #ifdef __cplusplus
